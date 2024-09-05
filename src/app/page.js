68aeb20c685313dd75163077ca0588946e5a6c0e
Mukultiwari03 <mukultiwari2032004@gmail.com>
2024-09-05 22:50:35 +0530

    2nd section done

--- src/app/page.js
+++ src/app/page.js
@@ -4,19 +4,23 @@ import { UserContext } from "../context/UserContext";
 import EnquiryForm from "@/components/HomePageComponents/EnquiryForm";
 import HeroImg from "@public/assets/images/HomePage/HeroImg.jpg";
 import Button from "@/components/Button";
+import happyTravels from "@public/assets/images/HomePage/happyTravels.jpg";
+import hat from "@public/assets/images/HomePage/hat.jpg";
+import passport from "@public/assets/images/HomePage/passport.jpg";
+
 export default function HomePage() {
   const { user, setUser } = useContext(UserContext);
 
   return (
     <>
-      <section className="relative h-[80rem] sm:h-[70rem] md:h-[65rem] lg:h-[65rem] bg-cover bg-center bg-no-repeat flex items-center justify-center">
+      <section className="relative h-[85rem] sm:h-[80rem] md:h-[75rem] lg:h-[65rem] bg-cover bg-center bg-no-repeat flex items-center justify-center">
         {/* Gradient Overlay */}
         <div
           className="absolute inset-0 bg-gradient-to-t from-[#000080] via-[#000080] to-[#000080] brightness-[0.65]"
           style={{
             backgroundImage: `url(${HeroImg.src})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
         ></div>
 
@@ -25,20 +29,19 @@ export default function HomePage() {
             <h3 className="uppercase font-medium text-xl">Abroad Educare</h3>
             <div className="mt-4 flex gap-5 ">
               {/* white line */}
-            <div className="w-[0.3rem]  bg-white"></div>
-            <div className="w-full lg:w-[80%]" >
-              <h1 className="leading-[4rem] lg:leading-none text-[3.4rem] text-5xl lg:text-7xl font-extralight">
-                A Global Exposure Consultancy for Students{" "}
-                <span className="font-bold">Schools and Colleges.</span>
-              </h1>
+              <div className="w-[0.3rem]  bg-white"></div>
+              <div className="w-full lg:w-[80%]">
+                <h1 className="leading-[4rem] lg:leading-none text-[3.4rem] text-5xl lg:text-7xl font-extralight">
+                  A Global Exposure Consultancy for Students{" "}
+                  <span className="font-bold">Schools and Colleges.</span>
+                </h1>
+              </div>
             </div>
-          
-          </div>
-          <p className="py-8 w-[70%]">
+            <p className="py-8 w-[70%]">
               Our forte lies in providing proper guidance and step-by-step
               method following to our clients for a seamless global journey.
             </p>
-            <Button text="discover more"/>
+            <Button text="discover more" />
           </div>
           <div className="w-full  lg:w-1/2 mt-8  lg:mt-0">
             {/* Enquiry form */}
@@ -48,26 +51,86 @@ export default function HomePage() {
       </section>
 
       {/* Immigration experts section */}
-      <section>
-        <div>
-          <p>Don’t Hesitate, Contact us for Better Help and Services. <span>Explore All Categories.</span></p>
+      <section className="flex flex-col-reverse lg:flex-row  w-full">
+        <div className="w-full lg:w-[50%] flex items-end">
+          <div className="ml-6 mt-8 lg:mt-0 lg:ml-10">
+            <p>
+              Don’t Hesitate, Contact us for Better Help and Services.{" "}
+              <span className="font-bold">Explore All Categories.</span>
+            </p>
+          </div>
         </div>
 
         {/* below div contain, Immigration Experts, Apply Online Visa, Immigration Experts */}
-        <div>
-          <div>
-            <div>
-              <h4>01.</h4>
-            </div>
-            <div>
-              <h2>Immigrations Experts</h2>
-            </div>
-            <div>
-              <p></p>
+        <div className="grid grid-cols-1 md:grid-cols-3 w-full lg:w-[50%]">
+          <div className="bg-white shadow shadow-gray-300 mix-blend-darken">
+            <div className="flex flex-col pb-10">
+              <div>
+                <div className="bg-blue-600 w-[fit-content] border-1 rounded-br-[1.25rem]">
+                  <h4 className="text-white px-[0.6rem] py-[0.3rem] font-semibold text-xl">
+                    01.
+                  </h4>
+                </div>
+              </div>
+              <div>
+                <h2 className="text-start ml-6 font-bold text-lg text-gray-800 py-4">
+                  Immigrations Experts
+                </h2>
+              </div>
+              <div className="flex justify-center items-center">
+                <p className="mx-[1.5rem] text-sm text-gray-800">
+                  We are providing the best service and consultancy for your
+                  delightful immigration experience.
+                </p>
+              </div>
             </div>
           </div>
-          <div></div>
-          <div></div>
+
+          <div className="bg-blue-600">
+            <div className="flex flex-col pb-10">
+              <div>
+                <div className="bg-white w-[fit-content] border-1 rounded-br-[1.25rem]">
+                  <h4 className="text-gray-800 px-[0.6rem] py-[0.3rem] font-semibold text-xl">
+                    02.
+                  </h4>
+                </div>
+              </div>
+              <div>
+                <h2 className="text-start ml-6 font-bold text-lg text-white py-4">
+                  Apply Online Visa
+                </h2>
+              </div>
+              <div className="flex justify-center">
+                <p className="mx-[1.5rem] text-sm text-white">
+                  We will help you providing proper guidance and step of the
+                  Visa application process.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-gray-800">
+            <div className="flex flex-col pb-10">
+              <div>
+                <div className="bg-blue-600 w-[fit-content] border-1 rounded-br-[1.25rem]">
+                  <h4 className="text-white px-[0.6rem] py-[0.3rem] font-semibold text-xl">
+                    03.
+                  </h4>
+                </div>
+              </div>
+              <div>
+                <h2 className="text-start ml-6 font-bold text-lg text-white py-4">
+                  Immigrations Experts
+                </h2>
+              </div>
+              <div className="flex justify-center items-center">
+                <p className="mx-[1.5rem] text-sm text-white">
+                  We will help you throughout the Immigration Process of your
+                  country of dreams.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
