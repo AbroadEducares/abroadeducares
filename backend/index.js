cb3346212e09888ed730f39383001d490269d948
Mukultiwari03 <mukultiwari2032004@gmail.com>
2025-01-01 20:31:39 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -10,17 +10,19 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 dbconnect();
 
-const userRoutes = require('./routes/user'); 
-const newsletterRoutes = require('./routes/NewsLetter'); // Use lowercase to stay consistent
-const agentRoutes = require('./routes/agent'); // Import the agent route
-const EduFairRoutes = require('./routes/EduFair');
-app.use('/api/v1', userRoutes);
-app.use('/api/v1', newsletterRoutes); // Consistently named route import
-app.use('/api/v1', agentRoutes); 
-app.use('/api/v1', EduFairRoutes);
+// const userRoutes = require('./routes/user'); 
+// const newsletterRoutes = require('./routes/NewsLetter'); // Use lowercase to stay consistent
+// const agentRoutes = require('./routes/agent'); // Import the agent route
+// const EduFairRoutes = require('./routes/EduFair');
+const allroutes = require('./routes/allroutes');
+app.use('/api/v1',allroutes);
+// app.use('/api/v1', userRoutes);
+// app.use('/api/v1', newsletterRoutes); // Consistently named route import
+// app.use('/api/v1', agentRoutes); 
+// app.use('/api/v1', EduFairRoutes);
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Edufair API, Hi1');
+    res.send('Welcome to the Abroad educares API, Hi1');
 }
 );
 app.listen(PORT, () => {
