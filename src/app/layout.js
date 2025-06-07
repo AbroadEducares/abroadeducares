0e7f2c9c5aa6ed767bf3dd1d1572fc71805b5785
Nemmen <aryankammboz@gmail.com>
2025-06-08 02:11:34 +0530

    layout changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,8 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer"
 import { UserProvider } from "./context/UserContext";
+import Script from "next/script"; // Add this import
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -16,12 +18,33 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '3896140077362846');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=3896140077362846&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body className={poppins.className}>
-      <UserProvider>
+        <UserProvider>
           <Navbar />
           {children}
           <Footer />
-          </UserProvider>
+        </UserProvider>
       </body>
     </html>
   );
